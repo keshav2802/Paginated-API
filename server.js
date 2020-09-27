978cf67fc9fb33e80a471053bbbeed97e7714d4a
keshavism28 <keshavism28@gmail.com>
2020-09-27 20:07:25 +0530

    created the users schema

--- server.js
+++ server.js
@@ -1,41 +1,6 @@
 const express = require('express');
 const app = express();
 
-const users = [
-  {id: 1, name: 'user 1'},
-  {id: 2, name: 'user 2'},
-  {id: 3, name: 'user 3'},
-  {id: 4, name: 'user 4'},
-  {id: 5, name: 'user 5'},
-  {id: 6, name: 'user 6'},
-  {id: 7, name: 'user 7'},
-  {id: 8, name: 'user 8'},
-  {id: 9, name: 'user 9'},
-  {id: 10, name: 'user 10'},
-  {id: 11, name: 'user 11'},
-  {id: 12, name: 'user 12'},
-  {id: 13, name: 'user 13'}
-];
-
-const posts = [
-  {id: 1, name: 'post 1'},
-  {id: 2, name: 'post 2'},
-  {id: 3, name: 'post 3'},
-  {id: 4, name: 'post 4'},
-  {id: 5, name: 'post 5'},
-  {id: 6, name: 'post 6'},
-  {id: 7, name: 'post 7'},
-  {id: 8, name: 'post 8'},
-  {id: 9, name: 'post 9'},
-  {id: 10, name: 'post 10'},
-  {id: 11, name: 'post 11'},
-  {id: 12, name: 'post 12'},
-  {id: 13, name: 'post 13'}
-];
-
-app.get('/posts', paginatedResults(posts), (req, res) => {
-  res.json(res.paginatedResults);
-})
 
 // Route for getting a list of users. We actually want to be able to pass in the url the page number and the number of users we want per page so that we can get only the resources that we want and not the whole list.This works great but usually when we have a paginated API, we actually want to return some more info from the API like about the next page and the previous page so that the user can know if there is a next page or not and same with the previous page. It just makes easier for the client using the API.
 

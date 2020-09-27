7465d1e468392992d782e9b175428c0acb16edcd
keshavism28 <keshavism28@gmail.com>
2020-09-27 19:19:34 +0530

    created a simple pagination

--- server.js
+++ server.js
@@ -17,9 +17,35 @@ const users = [
   {id: 13, name: 'user 13'}
 ];
 
-// route for getting a list of users
+// Route for getting a list of users. We actually want to be able to pass in the url the page number and the number of users we want per page so that we can get only the resources that we want and not the whole list.This works great but usually when we have a paginated API, we actually want to return some more info from the API like about the next page and the previous page so that the user can know if there is a next page or not and same with the previous page. It just makes easier for the client using the API.
+
 app.get('/users', (req, res) => {
-  res.json(users);
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const results = {};
+  
+  if(endIndex < users.length-1) {
+    results.next = {
+      page: page + 1,
+      limit: limit
+    };
+  }
+
+  if(startIndex > 0) {
+    results.previous = {
+      page: page - 1,
+      limit: limit
+    };
+  }
+  
+  results.results = users.slice(startIndex, endIndex);
+  
+  res.json(results);
 })
+
 
 app.listen(3000, () => console.log('server running on port 3000'));
